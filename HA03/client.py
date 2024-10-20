cf0b71c2d3ff8470b634fc0793c335dc7d797bf6
Matthew <mrw2003x@gmail.com>
2024-10-19 20:56:30 -0700

    Added file dunctionality

--- HA03/client.py
+++ HA03/client.py
@@ -1,4 +1,5 @@
 import socket
+import os
 
 # Initialize client socket
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Creating new socket Object
@@ -7,11 +8,49 @@ clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Creating new 
 clientSocket.connect(('localhost', 12345))
 # connecteing to local host
 
+def sendFile(fileName):
+    try:
+        with open(fileName, 'rb') as f:
+            data = f.read(1024)
+            while data:
+                clientSocket.send(data)
+                data = f.read(1024)
+        clientSocket.send(b'DONE')  # End signal
+        print(f"File '{fileName}' sent successfully.")
+    except FileNotFoundError:
+        print(f"File '{fileName}' not found.")
+
+def receiveFile(fileName):
+    with open(fileName, 'wb') as f:
+        while True:
+            data = clientSocket.recv(1024)
+            if data == b'DONE':  # End signal
+                break
+            f.write(data)
+    print(f"File '{fileName}' received successfully.")
+
 # Chat functionality
 while True:
+    print("Helpful Commands:\nexit - to leave\nlist - to display all files\nprint - 'file name/\n")
     clientMessage = input("Enter message to server: ")
-    clientSocket.send(clientMessage.encode('utf-8')) # we send our message after we encoded it
+   
+    if clientMessage.startswith('UPLOAD'):
+        _, filename = clientMessage.split(' ', 1)
+        clientSocket.send(clientMessage.encode('utf-8'))
+        sendFile(filename)
+
+    elif clientMessage.startswith('DOWNLOAD'):
+        _, filename = clientMessage.split(' ', 1)
+        clientSocket.send(clientMessage.encode('utf-8'))# we send our message after we encoded it
+        receiveFile(filename)
+
+    elif clientMessage == "list":
+        list
+
+    else:
+        clientSocket.send(clientMessage.encode('utf-8'))# we send our message after we encoded it
     
+        
     if clientMessage == 'exit':
         break
     
--- HA03/server.py
+++ HA03/server.py
@@ -1,4 +1,5 @@
 import socket
+import os
 
 # https://docs.python.org/3/library/socket.html
 
@@ -11,7 +12,29 @@ serverSocket.bind(('localhost', 12345)) # we are binding the socket to a spefic 
 # 12345 refers to our port number
 
 serverSocket.listen(1) # telling out socket to start listening
-# our parameter 1 refers to maximum number of client connectiosn the server can have
+# our parameter 1 refers to maximum number of client connectiosn the server can hav
+
+def receiveFile(fileName):
+    with open(fileName, 'wb') as f:
+        while True:
+            data = connection.recv(1024)
+            if data == b'DONE':  # End signal
+                break
+            f.write(data)
+    print(f"File '{fileName}' received successfully.")
+
+def sendFile(fileName):
+    if os.path.exists(fileName):
+        with open(fileName, 'rb') as f:
+            data = f.read(1024)
+            while data:
+                connection.send(data)
+                data = f.read(1024)
+        connection.send(b'DONE')  # End signal
+        print(f"File '{fileName}' sent successfully.")
+    else:
+        print("File not found!")
+
 
 print("Server is listening...")
 
@@ -19,16 +42,27 @@ connection, address = serverSocket.accept() # Accept client connection
 print(f"Connected by {address}")
 
 # Chat functionality
+
 while True:
     message = connection.recv(1024).decode('utf-8') 
     # We are waiting for a message from our client, we then recieve it and decode it
+    
+    print("Helpful Commands:\nexit - to leave\nUPLOAD - to upload a file\nDOWNLOAD - to download a file ")
 
     if message == 'exit':
         break
-    print("Client:", message)
-    
-    serverMessage = input("Enter message to client: ")
-    connection.send(serverMessage.encode('utf-8')) # we send our message back and encode it 
-
+    elif message.startswith('UPLOAD'):
+        _, filename = message.split(' ', 1)
+        print(f"Receiving file: {filename}")
+        receiveFile(filename)
+    elif message.startswith('DOWNLOAD'):
+        _, filename = message.split(' ', 1)
+        print(f"Sending file: {filename}")
+        sendFile(filename)
+    else:
+        print("Client:", message)
+        serverMessage = input("Enter message to client: ")
+        connection.send(serverMessage.encode('utf-8')) # we send our message back and encode it 
+        
 
 connection.close() # Close connection
